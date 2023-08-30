734dee52831eb9d57d669da038521898eee32c54
Ahmad <amq4319@gmail.com>
2023-08-30 23:50:50 +0300

    solve some basics exercises

---
+++ src/exercises/BarkingDog.java
@@ -0,0 +1,7 @@
+package exercises;
+
+public class BarkingDog {
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+        return barking && (hourOfDay < 8 || hourOfDay > 22) && (hourOfDay >= 0 && hourOfDay <= 23);
+    }
+}
