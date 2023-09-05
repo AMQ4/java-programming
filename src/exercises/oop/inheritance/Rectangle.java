dfd0984072d322c0617b66b491fbd4e275ba041b
Ahmad <amq4319@gmail.com>
2023-09-05 05:12:25 +0300

    Add Rectangle class for geometric rectangles

--- src/exercises/oop/inheritance/Rectangle.java
+++ src/exercises/oop/inheritance/Rectangle.java
@@ -1,2 +1,46 @@
-package exercises.oop.inheritance;public class Rectangle {
+package exercises.oop.inheritance;
+
+/**
+ * The `Rectangle` class represents a geometric rectangle with specified width and length.
+ */
+public class Rectangle {
+    protected double width, length;
+
+    /**
+     * Constructs a new `Rectangle` object with the specified width and length.
+     *
+     * @param width  The width of the rectangle. Must be greater than 0.
+     * @param length The length of the rectangle. Must be greater than 0.
+     */
+    public Rectangle(double width, double length) {
+        this.width = width > 0 ? width : 0;
+        this.length = length > 0 ? length : 0;
+    }
+
+    /**
+     * Gets the width of the rectangle.
+     *
+     * @return The width of the rectangle.
+     */
+    public double getWidth() {
+        return width;
+    }
+
+    /**
+     * Gets the length of the rectangle.
+     *
+     * @return The length of the rectangle.
+     */
+    public double getLength() {
+        return length;
+    }
+
+    /**
+     * Calculates and returns the area of the rectangle.
+     *
+     * @return The area of the rectangle.
+     */
+    public double getArea() {
+        return width * length;
+    }
 }
