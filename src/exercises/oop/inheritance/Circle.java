c19af4d18ac880b04725750c761b466c82f73d02
Ahmad <amq4319@gmail.com>
2023-09-05 05:11:00 +0300

    Add Cylinder class for geometric cylinders extending Circle

--- src/exercises/oop/inheritance/Circle.java
+++ src/exercises/oop/inheritance/Circle.java
@@ -1,17 +1,35 @@
-package challenges.oop.inheritance;
+package exercises.oop.inheritance;
 
+/**
+ * The `Circle` class represents a geometric circle with a specified radius.
+ */
 public class Circle {
     private double radius;
 
-    public Circle(double radius){
+    /**
+     * Constructs a new `Circle` object with the specified radius.
+     *
+     * @param radius The radius of the circle. Must be greater than 0.
+     */
+    public Circle(double radius) {
         this.radius = radius > 0 ? radius : 0;
     }
 
-    public double getRadius(){
+    /**
+     * Gets the radius of the circle.
+     *
+     * @return The radius of the circle.
+     */
+    public double getRadius() {
         return radius;
     }
 
-    public double getArea(){
+    /**
+     * Calculates and returns the area of the circle.
+     *
+     * @return The area of the circle.
+     */
+    public double getArea() {
         return radius * radius * Math.PI;
     }
 }
--- src/exercises/oop/inheritance/Cylinder.java
+++ src/exercises/oop/inheritance/Cylinder.java
@@ -1,18 +1,38 @@
-package challenges.oop.inheritance;
+package exercises.oop.inheritance;
 
-public class Cylinder extends Circle{
+/**
+ * The `Cylinder` class represents a geometric cylinder, extending the functionality of a circle to include height.
+ */
+public class Cylinder extends Circle {
     private double height;
 
-    public Cylinder(double radius, double height){
+    /**
+     * Constructs a new `Cylinder` object with the specified radius and height.
+     *
+     * @param radius The radius of the cylinder's base. Must be greater than 0.
+     * @param height The height of the cylinder. Must be greater than 0.
+     */
+    public Cylinder(double radius, double height) {
         super(radius);
         this.height = height > 0 ? height : 0;
     }
 
-    public double getHeight(){
+    /**
+     * Gets the height of the cylinder.
+     *
+     * @return The height of the cylinder.
+     */
+    public double getHeight() {
         return height;
     }
 
-    public double getVolume(){
+    /**
+     * Calculates and returns the volume of the cylinder.
+     * The volume is calculated as the product of the base area (π * radius^2) and the height.
+     *
+     * @return The volume of the cylinder.
+     */
+    public double getVolume() {
         return getArea() * height;
     }
 }
