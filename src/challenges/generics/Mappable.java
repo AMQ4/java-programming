d4f43475c70614881ecd26a894aacdb6d9741cfb
Ahmad <amq4319@gmail.com>
2023-09-27 13:15:41 +0300

    Add Mappable interface for objects that can be rendered as strings

---
+++ src/challenges/generics/Mappable.java
@@ -0,0 +1,13 @@
+package challenges.generics;
+
+/**
+ * Represents objects that can be rendered as strings.
+ */
+public interface Mappable {
+    /**
+     * Renders the object as a string.
+     *
+     * @return A string representation of the object.
+     */
+    String render();
+}
